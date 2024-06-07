5757dd4d656db4a45b3813b38e22655bd41f8ba4
Victarr75 <korirvictor75@gmail.com>
2024-06-07 15:30:18 +0300

    correct

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * is_leaf_node - Checks if a given node is a leaf in a binary tree.
- * @node: A pointer to the node to check.
+ * binary_tree_is_leaf - Checks if a node is a leaf
  *
- * Return: 1 if the node is a leaf, 0 otherwise.
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
  */
-int is_leaf_node(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
-	return (node->left == NULL && node->right == NULL);
+	return (node->left == NULL && node->right == NULL ? 1 : 0);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * is_root_node - Checks if a given node is a root in a binary tree.
- * @node: A pointer to the node to check.
+ * binary_tree_is_root - Checks if a given node is a root
  *
- * Return: 1 if the node is a root, 0 otherwise.
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if node is a root, 0 otherwise
  */
-int is_root_node(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
 
-	return (node->parent == NULL);
+	return (node->parent == NULL ? 1 : 0);
 }
