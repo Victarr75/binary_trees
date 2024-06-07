6812a7c11fc032a11f351e9ecc5fc40bde50a07b
Victarr75 <korirvictor75@gmail.com>
2024-06-07 14:58:03 +0300

    binary trees

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,22 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	const binary_tree_t *current = tree;
-	const binary_tree_t *stack[100]; /* Assume max height of the tree is 100 */
-	size_t top = 0;
-
-	while (current || top > 0)
-	{
-		while (current)
-		{
-			func(current->n);
-
-			if (current->right)
-				stack[top++] = current->right;
-
-			current = current->left;
-		}
-
-		current = stack[--top];
-	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
+
